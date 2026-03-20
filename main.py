cee5de294e4a693093e39040ba9d1a8d06c638b0
Sunny Raphael <ifeoluwaraphael0@email.com>
2026-03-20 11:12:24 +0100

    fix CORS for production

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://whispr-app.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
